57596b081547a71e38987d2f012aa9b825619ab8
Gautam Mittal <gautam@mittal.net>
2018-08-10 15:07:55 -0700

    Reinitialize everything.

--- main.py
+++ main.py
@@ -1,130 +1,96 @@
-import gym
-import numpy as np
 import random
-import scipy
+import gym
+import math
+import numpy as np
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Dense
 from keras.optimizers import Adam
-from keras import backend as K
 
-EPISODES = 5000
+def preprocess(I):
+    I = I[35:195] # crop
+    I = I[::2,::2,0] # downsample by factor of 2
+    I[I == 144] = 0 # erase background (background type 1)
+    I[I == 109] = 0 # erase background (background type 2)
+    I[I != 0] = 1
+    return I.astype(np.float).ravel()
 
-def huber_loss(target, prediction):
-    return K.mean(K.sqrt(1+K.square(prediction-target))-1, axis=-1)
+def get_epsilon(e):
+    return max(EPSILON_MIN, min(EPSILON, 1.0 - math.log10((e + 1) * EPSILON_DECAY)))
 
-def preprocess_state(state):
-    return np.reshape(state, [1, 4])
+def build_model():
+    model = Sequential()
+    model.add(Dense(60, input_dim=STATE_SIZE, activation='relu'))
+    model.add(Dense(40, activation='relu'))
+    model.add(Dense(ACTION_SIZE, activation='linear'))
+    model.compile(loss='mse',
+                  optimizer=Adam(lr=LEARNING_RATE))
+    return model
 
-class Memory:
-    def __init__(self, size):
-        self.buffer = deque(maxlen=size)
+def remember(state, action, reward, next_state, done):
+    MEMORY.append((state, action, reward, next_state, done))
 
-    def add(self, sample):
-        self.buffer.append(sample)
+def act(state):
+    if np.random.rand() <= EPSILON:
+        return random.randrange(ACTION_SIZE)
+    act_values = model.predict(state)
+    return np.argmax(act_values[0])  # returns action
 
-    def sample(self, batch_size):
-        return random.sample(self.buffer, batch_size)
+def replay(batch_size):
+    minibatch = random.sample(MEMORY, batch_size)
+    for state, action, reward, next_state, done in minibatch:
+        target = reward
+        if not done:
+            target = (reward + GAMMA *
+                      np.amax(model.predict(next_state)[0]))
+        target_f = model.predict(state)
+        target_f[0][action] = target
+        model.fit(state, target_f, epochs=1, verbose=0)
 
-    def __len__(self):
-        return len(self.buffer)
 
-class Agent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = Memory(2000)
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_min = 0.02
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
-        self.model = self._build_model()
-        self.target_model = self._build_model()
-        self.update_target_model()
+env = gym.make('CartPole-v1')
 
-    def _build_model(self):
-        model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='tanh'))
-        model.add(Dense(48, activation='tanh'))
-        model.add(Dense(units=self.action_size, activation='linear'))
-        model.compile(loss=huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
-        return model
+EPISODES = 1000
+LEARNING_RATE = 1e-4
+BATCH_SIZE = 32
 
-    def update_target_model(self):
-        self.target_model.set_weights(self.model.get_weights())
+STATE_SIZE = 4#80 * 80
+ACTION_SIZE = env.action_space.n
+MEMORY = deque(maxlen=2000)
+GAMMA = 0.99
+EPSILON = 1.0
+EPSILON_MIN = 0.01
+EPSILON_DECAY=0.9995
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.add((state, action, reward, next_state, done))
+model = build_model()
 
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
-
-    def replay(self, batch_size):
-        minibatch = self.memory.sample(batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = self.model.predict(state)
-            if done:
-                target[0][action] = reward
-            else:
-                t = self.target_model.predict(next_state)[0]
-                target[0][action] = reward + self.gamma * np.amax(t)
-            self.model.fit(state, target, epochs=1, verbose=0)
-        # Epsilon decay
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def load(self, name):
-        self.model.load_weights(name)
-
-    def save(self, name):
-        self.model.save_weights(name)
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
-    env._max_episode_steps = None
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = Agent(state_size, action_size)
-
-    done = False
-    batch_size = 32
-
-    frames = 0
+    # Monitoring
+    running_avg = deque(maxlen=100)
 
     for e in range(EPISODES):
         state = env.reset()
-        state = preprocess_state(state)
-
-        total_reward = 0
-
-        while True:
-            frames += 1
-            # env.render()
-            action = agent.act(state)
+        state = np.reshape(state, [1, STATE_SIZE])
+        R = 0
+        done = False
+        while not done:
+            env.render()
+            action = act(state)
             next_state, reward, done, _ = env.step(action)
             reward = reward if not done else -10
-            next_state = preprocess_state(next_state)
-            agent.remember(state, action, reward, next_state, done)
-
+            next_state = np.reshape(next_state, [1, STATE_SIZE])
+            remember(state, action, reward, next_state, done)
             state = next_state
-            total_reward += reward
 
-            if frames > 100:
-                frames = 0
-                agent.update_target_model()
-
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
+            R += reward
 
             if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, total_reward, agent.epsilon))
+                running_avg.append(R)
+                avg = round(sum(running_avg) / len(running_avg))
+                print("episode: {}/{}, score: {}, e: {:.2}, avg (100 eps): {}"
+                      .format(e, EPISODES, R, EPSILON, avg))
+                EPSILON = get_epsilon(e)
                 break
+            if (e % 10 == 0 and e > 0):
+                replay(BATCH_SIZE)
